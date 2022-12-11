a26c092fb3ce37941435d1d9e5d6884b08549c28
Breno Nahuz <bnahuz@live.com>
2022-12-11 12:50:51 -0300

    first commit

---
+++ tradutor/TokenType.java
@@ -0,0 +1,14 @@
+package tradutor;
+
+public enum TokenType {
+    KEYWORD,
+    SYMBOL,
+    IDENTIFYER,
+    INTCONST,
+    STRINGCONST,
+    NUMBER,
+    IDENT,
+    ILLEGAL,
+    PLUS,MINUS,PRINT,LET,SEMICOLON, EQ,
+    EOF
+}
